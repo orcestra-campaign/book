32ee297996912bfd5ff63006ee4fb980e4144a3c
Lukas Kluft <lukas.kluft@gmail.com>
2024-07-15 11:41:46 +0200

    Add `badges` role to add a row of badges to a flight report

--- orcestra_book/_ext/category_role.py
+++ orcestra_book/_ext/category_role.py
@@ -1,42 +1,63 @@
 #!/usr/bin/env python3
 from docutils import nodes
 
+from frontmatter import Frontmatter
 from sphinx.util.docutils import SphinxRole
+
+
+def create_badge(cat_id):
+    """Return an HTML node based on a category id."""
+    cat_tier = {
+       # Important objectives
+       "ec_under": "a",
+       "ec_track": "a",
+       "two_circ": "a",
+       "itcz_circ": "a",
+       # Semi-important objectives
+       "south": "b",
+       "atr_circ": "b",
+       "atr_over": "b",
+       "sar": "b",
+       "pace": "b",
+       "gpm": "b",
+       "mindels": "b",
+       "cloud_circ": "b",
+       "curtain": "b",
+       "radar": "b",
+       # Nice-to-have
+       "cirrus": "c",
+    }.get(cat_id, "unknown")
+
+    node = nodes.raw(
+        text=f'<span class="badge cat-{cat_tier} cat-{cat_id}">{cat_id}</span>',
+        format="html",
+    )
+
+    return node
 
 
 class CategoryRole(SphinxRole):
     def run(self):
-        tier = {
-           # Important objectives
-           "ec_under": "a",
-           "ec_track": "a",
-           "two_circ": "a",
-           "itcz_circ": "a",
-           # Semi-important objectives
-           "south": "b",
-           "atr_circ": "b",
-           "atr_over": "b",
-           "sar": "b",
-           "pace": "b",
-           "gpm": "b",
-           "mindels": "b",
-           "cloud_circ": "b",
-           "curtain": "b",
-           "radar": "b",
-           # Nice-to-have
-           "cirrus": "c",
-        }.get(self.text, "unknown")
-
-        node = nodes.raw(
-            text=f'<span class="badge cat-{tier} cat-{self.text}">{self.text}</span>',
-            format="html",
-        )
+        node = create_badge(self.text)
 
         return [node], []
 
 
+class BadgesRole(SphinxRole):
+    def run(self):
+        """(Re-)parse the frontmatter of the current document and create badges."""
+        categories = Frontmatter.read_file(
+            self.env.doc2path(self.env.docname)
+        )["attributes"].get("categories", [])
+
+        nodes = [create_badge(cat_id) for cat_id in categories]
+
+        return nodes, []
+
+
 def setup(app):
     app.add_role("cat", CategoryRole())
+    app.add_role("badges", BadgesRole())
 
     return {
         "version": "0.1",
