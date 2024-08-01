5f195fc8ca0f860d11f82c4a6514de7cff819ee9
Lukas Kluft <lukas.kluft@gmail.com>
2024-08-01 12:27:36 +0200

    Refactor creation of overview tables

--- orcestra_book/_ext/reports.py
+++ orcestra_book/_ext/reports.py
@@ -1,67 +1,71 @@
-#!/usr/bin/env python3
-import os
 import pathlib
-import subprocess
-from datetime import datetime
-from docutils import nodes
-from functools import lru_cache, partial
+import re
+from collections import defaultdict
+from functools import lru_cache
 
 import yaml
 from jinja2 import Template
-from sphinx.util.docutils import SphinxRole
 
 
 @lru_cache
-def load_frontmatter(path, derive_flight=False):
+def load_frontmatter(path):
+    """Load and return the front matter section of a YAML file."""
+    print(path)
     with open(path, "r") as fp:
         frontmatter = next(yaml.safe_load_all(fp))
 
-    if derive_flight:
-        takeoff = frontmatter["takeoff"]
-        landing = frontmatter["landing"]
-
-        frontmatter["expr_date"] = takeoff.strftime("%d %B %Y")
-        frontmatter["expr_takeoff"] = takeoff.strftime("%X")
-        frontmatter["expr_landing"] = landing.strftime("%X")
-        frontmatter["expr_categories"] = map(
-            lambda s: f"{{flight-cat}}`{s}`", frontmatter.get("categories", [])
-        )
+    frontmatter["filepath"] = path.as_posix()
 
     return frontmatter
 
 
-def collect_halo_refs(src, flight_id):
-    refs =  ", ".join(
-        f"[{t}](../{t}s/{flight_id})" for t in ("plan", "report")
-        if (src / f"{t}s" / f"{flight_id}.md").is_file()
-    )
-
-    return f"{flight_id} ({refs})"
+def fpath2id(fpath):
+    """Extract the ID (e.g. HALO-20240801a) from a given file path."""
+    return pathlib.Path(fpath).stem
 
 
-def write_flight_table(app):
-    src = pathlib.Path(app.srcdir)
-    flights = (src / "reports").glob("HALO-[0-9]*[a-z].md")
+def collect_all_metadata(src, subdirs=("plans", "reports"), pattern="*.md"):
+    """Collect front matter from Markdown files in various given locations."""
+    metadata = defaultdict(dict)
+    for subdir in subdirs:
+        for src_file in (src / subdir).glob(pattern):
+            subdir = (
+                subdir[:-1] if subdir.endswith("s") else subdir
+            )  # e.g. plans -> plan
+            metadata[fpath2id(src_file)][subdir] = load_frontmatter(src_file)
 
-    func = partial(load_frontmatter, derive_flight=True)
-    frontmatters = {fm["flight_id"]: fm for fm in map(func, sorted(flights))}
+    return metadata
 
-    for flight_id in frontmatters:
-        frontmatters[flight_id]["expr_refs"] = collect_halo_refs(src, flight_id)
 
-    with open(src / "_templates" / "operation_halo.md", "r") as fp:
-        templ = fp.read()
+def consolidate_metadata(src, metadata):
+    """Merge duplicated data from flight plans and reports."""
+    latest_source = "report" if "report" in metadata else "plan"
 
-    with open(src / "operation" / "halo.md", "w") as fp:
-        t = Template(templ)
-        fp.write(t.render(flights=frontmatters))
+    for key in ("takeoff", "landing", "pi", "categories", "nickname"):
+        metadata[key] = metadata[latest_source][key]
+
+    # Collect relative Sphinx links to flight plans and reports
+    refs = []
+    for k, v in metadata.items():
+        if isinstance(v, dict) and "filepath" in v:
+            relpath = pathlib.Path(v["filepath"]).relative_to(
+                src / "operation", walk_up=True
+            )
+            refs.append(f"[{k}]({relpath})")
+
+    metadata["refs"] = refs
+
+    return metadata
 
 
 def write_ship_table(app):
+    """Collect all reports from RV METEOR and create an overview table."""
     src = pathlib.Path(app.srcdir)
-    reports = (src / "reports").glob("METEOR-[0-9]*.md")
+    metadata = collect_all_metadata(src)
 
-    frontmatters = {fm["report_id"]: fm for fm in map(load_frontmatter, sorted(reports))}
+    regex = re.compile("METEOR-[0-9]*")
+    # For now, only shipd **reports** are expected. May need to be adapted in future.
+    frontmatters = {k: v["report"] for k, v in metadata.items() if regex.match(k)}
 
     with open(src / "_templates" / "operation_rvmeteor.md", "r") as fp:
         templ = fp.read()
@@ -69,6 +73,26 @@ def write_ship_table(app):
     with open(src / "operation" / "rvmeteor.md", "w") as fp:
         t = Template(templ)
         fp.write(t.render(reports=frontmatters))
+
+
+def write_flight_table(app=None):
+    """Collect all reports from HALO and create an overview table."""
+    # src = pathlib.Path(app.srcdir)
+    src = pathlib.Path("orcestra_book/")
+    metadata = collect_all_metadata(src)
+
+    regex = re.compile("HALO-[0-9]*[a-z]")
+    frontmatters = {
+        k: consolidate_metadata(src, v) for k, v in metadata.items() if regex.match(k)
+    }
+    print(frontmatters["HALO-20240810a"]["refs"])
+
+    with open(src / "_templates" / "operation_halo.md", "r") as fp:
+        templ = fp.read()
+
+    with open(src / "operation" / "halo.md", "w") as fp:
+        t = Template(templ)
+        fp.write(t.render(flights=frontmatters))
 
 
 def setup(app):
--- orcestra_book/_templates/operation_halo.md
+++ orcestra_book/_templates/operation_halo.md
@@ -4,5 +4,11 @@
 Flight-ID | Date | Takeoff | Landing | PI | Nickname | Categories
 --- | --- | --- | --- | --- | --- | ---
 {% for k, v in flights.items() -%}
-{{ v["expr_refs"] }} | {{ v["expr_date"] }} | {{ v["expr_takeoff"] }} | {{ v["expr_landing"] }} | {{ v["pi"] }} | {{ v["nickname"] }} | {{ v["expr_categories"]|join(' ') }}
+{% set c1 = v["refs"]|join(", ") -%}
+{% set c2 = v["takeoff"].strftime("%d %B %Y") -%}
+{% set c3 = v["takeoff"].strftime("%X") -%}
+{% set c4 = v["landing"].strftime("%X") -%}
+{% set c5 = v["pi"] -%}
+{% set c6 = v["nickname"] -%}
+{{ k }} ({{ c1 }}) | {{ c2 }} | {{ c3 }} | {{ c4 }} | {{ c5 }} | {{ c6 }} | {% for cat in v["categories"] %}{flight-cat}`{{cat}}` {% endfor %}
 {% endfor -%}
